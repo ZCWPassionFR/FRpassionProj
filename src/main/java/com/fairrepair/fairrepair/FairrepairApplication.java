225a078422259086e8989728622e6de16f402093
James <jhjx92@gmail.com>
2023-05-25 16:09:36 -0400

    adding project

---
+++ src/main/java/com/fairrepair/fairrepair/FairrepairApplication.java
@@ -0,0 +1,13 @@
+package com.fairrepair.fairrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairrepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairrepairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fairrepair/fairrepair/FairrepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairrepair.fairrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairrepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
